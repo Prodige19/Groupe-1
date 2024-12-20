17be8e435dce86d2b74a0cef7a8eb2019c0e8641
Prodige Manakel OKO-ALPHOKO <prodigeoko19@gmail.com>
2024-12-20 09:15:51 +0000

    20/12/2024 09:15

---
+++ Calculatrice.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+//Declaration des variables 
+
+//Fonction addition (Prodige)
+
+//Fonction multiplication (Fanta)
+
+//Fonction soustraction (Maeva)
+
+//Fonction Divition (Sobour)
+
+main(){
+    
+}
